cf11de217c9a8f475f785c72b727fbb97a6cf149
douglass-blip <douglas.jordao@sempreceub.com>
2026-06-11 09:52:16 -0300

    Update LivroRepository.java

--- src/main/java/com/biblioteca/repository/LivroRepository.java
+++ src/main/java/com/biblioteca/repository/LivroRepository.java
@@ -4,11 +4,8 @@ import com.biblioteca.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repositório JPA para a entidade Livro.
- * Herda os métodos CRUD automaticamente do JpaRepository.
- */
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
-    // Métodos CRUD já disponíveis: save, findById, findAll, deleteById, etc.
+   
 }
