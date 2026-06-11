44c7674bb5ae64c984daf0801efed8d745101c64
douglass-blip <douglas.jordao@sempreceub.com>
2026-06-11 09:53:55 -0300

    Update Editora.java

--- src/main/java/com/biblioteca/model/Editora.java
+++ src/main/java/com/biblioteca/model/Editora.java
@@ -3,10 +3,7 @@ package com.biblioteca.model;
 import jakarta.persistence.*;
 import java.util.List;
 
-/**
- * Entidade que representa uma Editora no sistema de biblioteca.
- * Uma editora pode publicar vários livros.
- */
+
 @Entity
 @Table(name = "editoras")
 public class Editora {
@@ -24,21 +21,21 @@ public class Editora {
     @Column(nullable = false)
     private String cidade;
 
-    // Uma editora pode ter vários livros
+   
     @OneToMany(mappedBy = "editora", cascade = CascadeType.ALL)
     private List<Livro> livros;
 
-    // Construtor padrão (obrigatório para JPA)
+    
     public Editora() {}
 
-    // Construtor com parâmetros
+   
     public Editora(String nome, String cnpj, String cidade) {
         this.nome = nome;
         this.cnpj = cnpj;
         this.cidade = cidade;
     }
 
-    // Getters e Setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
