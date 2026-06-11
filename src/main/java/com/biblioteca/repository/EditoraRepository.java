6e77bbea5a76a0bc15c384eabddcec8158523450
douglass-blip <douglas.jordao@sempreceub.com>
2026-06-11 09:52:29 -0300

    Update EditoraRepository.java

--- src/main/java/com/biblioteca/repository/EditoraRepository.java
+++ src/main/java/com/biblioteca/repository/EditoraRepository.java
@@ -4,11 +4,8 @@ import com.biblioteca.model.Editora;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repositório JPA para a entidade Editora.
- * Herda os métodos CRUD automaticamente do JpaRepository.
- */
+
 @Repository
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
-    // Métodos CRUD já disponíveis: save, findById, findAll, deleteById, etc.
+   
 }
