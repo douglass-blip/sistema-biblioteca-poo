36e5ee886f7317c491e764483d5a09a31457e2e0
douglass-blip <douglas.jordao@sempreceub.com>
2026-06-11 09:49:39 -0300

    Update BibliotecaApplication.java

--- src/main/java/com/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/biblioteca/BibliotecaApplication.java
@@ -3,10 +3,7 @@ package com.biblioteca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Classe principal do Sistema de Biblioteca.
- * Inicializa a aplicação Spring Boot.
- */
+
 @SpringBootApplication
 public class BibliotecaApplication {
 
